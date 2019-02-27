06bcbb407feaf032ee2c4f15358e68e1658a6fb1
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-02-27 18:46:04 -0500

    Pep

--- image.py
+++ image.py
@@ -1,30 +1,34 @@
 from skimage import io
 import matplotlib.pyplot as plt
 import numpy as np
+
 np.set_printoptions(precision=2)
 np.set_printoptions(suppress=True)
+
+
 def open_image_as_ndarray(filename):
     img = io.imread(filename)
     h = img.shape[0]
     w = img.shape[1]
-    assert h%16 == 0 and w%16 == 0, "We're only doing multiples of 16 for width and height"
+    assert h % 16 == 0 and w % 16 == 0, "We're only doing multiples of 16 for width and height"
     return img
+
 
 def get_test_image():
     # RECAP : Opening the image and splitting it into blocks
     img = io.imread('input_image.png')
     h = img.shape[0]
     w = img.shape[1]
-    img = img[:h - h%16, :w-w%16,:]
+    img = img[:h - h % 16, :w - w % 16, :]
     return img
 
 
 if __name__ == "__main__":
-    img = get_test_image()
-    red = img[:,:,0]
-    green = img[:,:,1]
-    blue = img[:,:,2]
-    plt.imshow(img)
+    my_img = get_test_image()
+    red = my_img[:, :, 0]
+    green = my_img[:, :, 1]
+    blue = my_img[:, :, 2]
+    plt.imshow(my_img)
     plt.show()
     plt.imshow(red, cmap=plt.get_cmap('Reds'))
     plt.show()
@@ -32,5 +36,3 @@ if __name__ == "__main__":
     plt.show()
     plt.imshow(blue, cmap=plt.get_cmap('Blues'))
     plt.show()
-
-
--- zigzag.py
+++ zigzag.py
@@ -1,14 +1,14 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-def zig_zag_block(block):
 
+def zig_zag_block(block):
     line = []
     for ind in zig_zag_indices(block.shape[0], block.shape[1]):
         line.append(block[ind])
 
     return np.array(line)
+
 
 def zig_zag_blocks(blocks):
     n_blocks_h, n_blocks_w, block_h, block_w = blocks.shape
@@ -18,19 +18,19 @@ def zig_zag_blocks(blocks):
         i_nhh = i * block_wh * n_blocks_w
         for j in range(n_blocks_w):
             start = i_nhh + j * block_h
-            zig_zagged[start: start + block_wh] = zig_zag_block(blocks[i,j,:,:])
+            zig_zagged[start: start + block_wh] = zig_zag_block(blocks[i, j, :, :])
             pass
 
     return zig_zagged
 
 
 def un_zig_zag_block(line, h, w):
-    import math
     block = np.zeros((h, w))
     for ind, k in zip(zig_zag_indices(h, w), line):
         block[ind] = k
 
     return block
+
 
 def un_zig_zag_blocks(line, n_blocks_h, n_blocks_w, block_h, block_w):
     blocks = np.zeros((n_blocks_h, n_blocks_w, block_h, block_w))
@@ -39,7 +39,7 @@ def un_zig_zag_blocks(line, n_blocks_h, n_blocks_w, block_h, block_w):
         i_nhh = i * block_wh * n_blocks_w
         for j in range(n_blocks_w):
             start = i_nhh + j * block_h
-            blocks[i,j,:,:] = un_zig_zag_block(line[start: start + block_wh], block_h, block_w)
+            blocks[i, j, :, :] = un_zig_zag_block(line[start: start + block_wh], block_h, block_w)
 
     return blocks
 
@@ -49,9 +49,9 @@ def zig_zag_indices(h, w):
 
     i, j = 0, 0
 
-    while i+1 < h and j+1 < w:
+    while i + 1 < h and j + 1 < w:
 
-        yield (i,j)
+        yield (i, j)
 
         if i == 0 and j % 2 == 0:
             j += 1
@@ -67,7 +67,7 @@ def zig_zag_indices(h, w):
 
     while i < h and j < w:
 
-        yield (i,j)
+        yield (i, j)
 
         if i + 1 == h and j % 2 == 0:
             j += 1
@@ -88,29 +88,18 @@ if __name__ == "__main__":
 
     N = 12
 
-    a = un_zig_zag_block(np.array(range(N*N)),N,N)
-    def zig_zag_one_block(block):
-        line = np.zeros(block.shape[0] * block.shape[1])
-
-    a = np.zeros((N,N))
-    i = 0
-    for ind in zig_zag_indices(N, N):
-        a[ind] = i
-        i += 1
+    a = un_zig_zag_block(np.array(range(N * N)), N, N)
 
     print(a.astype('int'))
     plt.imshow(a, cmap=plt.get_cmap('gray'))
     plt.show()
 
-
-    big_a = np.zeros((2,1,N,N))
-    big_a[0,0,:,:] = a[:,:]
-    big_a[1,0,:,:] = -a
-
-
+    big_a = np.zeros((2, 1, N, N))
+    big_a[0, 0, :, :] = a[:, :]
+    big_a[1, 0, :, :] = -a
 
     zz = zig_zag_block(a)
-    un_zzd = un_zig_zag_block(zz,N,N).astype('int')
+    un_zzd = un_zig_zag_block(zz, N, N).astype('int')
     assert un_zzd.all() == a.astype('int').all(), "Didn't work"
 
     zz = zig_zag_blocks(big_a)
