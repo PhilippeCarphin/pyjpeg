f966c8a70b3b815cfe5b480e03695d410e6a0ac6
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-03-01 03:04:31 -0500

    Everything works great except for huffman

--- pyjpeg.py
+++ pyjpeg.py
@@ -9,43 +9,93 @@ import huffman_8770 as huffman
 import numpy as np
 
 import matplotlib.pyplot as plt
-subsample = False
+subsample = True
 use_dct = True
-def encode(filename):
-    rgb_img = image.open_image_as_ndarray(filename)
+use_quantize = True
+use_zigzag = True
+use_huffman = False
 
+image_shape = None
+blocks_shape = None
+subsampled_shape = None
+
+def encode(filename):
+
+    global subsampled_shape
+    global blocks_shape
+
+    rgb_img = image.open_image_as_ndarray(filename)
     ycbcr_img = ycbcr.rgb2ycbcr(rgb_img)
 
-    subsampled = subsampling.scheme_subsample(ycbcr_img, (4,4,4))
-    blocks_Y_ss = blocks.split_NxN(subsampled['Y'])
-    blocks_Cb_ss = blocks.split_NxN(subsampled['Cb'])
-    blocks_Cr_ss = blocks.split_NxN(subsampled['Cr'])
     ycbcr_blocks = blocks.split_NxN(ycbcr_img)
     blocks_Y = ycbcr_blocks[:,:,:,:,0]
     blocks_Cb = ycbcr_blocks[:,:,:,:,1]
     blocks_Cr = ycbcr_blocks[:,:,:,:,2]
 
-    assert blocks_Y_ss.shape == blocks_Y.shape
     if subsample:
+
+        subsampled = subsampling.scheme_subsample(ycbcr_img, (4,2,0))
+
+        blocks_Y_ss = blocks.split_NxN(subsampled['Y'])
+        blocks_Cb_ss = blocks.split_NxN(subsampled['Cb'])
+        blocks_Cr_ss = blocks.split_NxN(subsampled['Cr'])
+
         blocks_Y = blocks_Y_ss
         blocks_Cb = blocks_Cb_ss
         blocks_Cr = blocks_Cr_ss
+
+        assert blocks_Y_ss.shape == blocks_Y.shape
+
+    subsampled_shape = blocks_Cb.shape
+    blocks_shape = blocks_Y_ss.shape
 
     if use_dct:
         blocks_Y = dct.dct_encode_blocks(blocks_Y)
         blocks_Cb = dct.dct_encode_blocks(blocks_Cb)
         blocks_Cr = dct.dct_encode_blocks(blocks_Cr)
 
-    if quantize:
-        blocks_Y = quantize.quantize_blocks(blocks_Y)
-        blocks_Cb = quantize.quantize_blocks(blocks_Cb)
-        blocks_Cr = quantize.quantize_blocks(blocks_Cr)
+        if use_quantize:
+            blocks_Y = quantize.quantize_blocks(blocks_Y)
+            blocks_Cb = quantize.quantize_blocks(blocks_Cb)
+            blocks_Cr = quantize.quantize_blocks(blocks_Cr)
 
+            if use_zigzag:
+                zigzag_Y = zigzag.zig_zag_blocks(blocks_Y)
+                zigzag_Cb = zigzag.zig_zag_blocks(blocks_Cb)
+                zigzag_Cr = zigzag.zig_zag_blocks(blocks_Cr)
+                if use_huffman:
+                    huffed_Y = huffman.huffman_encode(zigzag_Y.astype('uint8'))
+                    huffed_Cb = huffman.huffman_encode(zigzag_Cb.astype('uint8'))
+                    huffed_Cr = huffman.huffman_encode(zigzag_Cr.astype('uint8'))
+                    return {
+                        'data': (huffed_Y, huffed_Cb, huffed_Cr),
+                        'subsampling_scheme': (4,2,0),
+                        'img_shape': ycbcr_img.shape,
+                        'Y_shape': blocks_Y.shape,
+                        'Cr_shape': blocks_Cr.shape}
+                else:
+                    return (zigzag_Y, zigzag_Cb, zigzag_Cr)
 
-    return (blocks_Y, blocks_Cb, blocks_Cr)
+        return (blocks_Y, blocks_Cb, blocks_Cr)
 
 def decode(encoded):
-    blocks_Y, blocks_Cb, blocks_Cr = encoded
+    if not use_huffman:
+        blocks_Y, blocks_Cb, blocks_Cr = encoded
+
+    if use_zigzag:
+
+        if use_huffman:
+            huffed_Y, huffed_Cb, huffed_Cr = encoded['data']
+
+            zigzag_Y = np.uint8(huffman.huffman_decode(huffed_Y['data'], huffed_Y['codebook'])).astype('float')
+            zigzag_Cb = np.uint8(huffman.huffman_decode(huffed_Cb['data'], huffed_Cb['codebook'])).astype('float')
+            zigzag_Cr = np.uint8(huffman.huffman_decode(huffed_Cr['data'], huffed_Cr['codebook'])).astype('float')
+        else:
+            zigzag_Y, zigzag_Cb, zigzag_Cr = encoded
+
+        blocks_Y = zigzag.un_zig_zag_blocks(zigzag_Y, blocks_shape)
+        blocks_Cb = zigzag.un_zig_zag_blocks(zigzag_Cb, subsampled_shape)
+        blocks_Cr = zigzag.un_zig_zag_blocks(zigzag_Cr, subsampled_shape)
 
     if use_dct:
         blocks_Y = dct.dct_decode_blocks(blocks_Y)
@@ -63,7 +113,7 @@ def decode(encoded):
             'Y': Y_channel,
             'Cb': Cb_channel,
             'Cr': Cr_channel,
-            'scheme': (4,4,4)
+            'scheme': (4,2,0)
         })
     else:
         ycbcr_img = np.zeros(Y_channel.shape + (3,))
